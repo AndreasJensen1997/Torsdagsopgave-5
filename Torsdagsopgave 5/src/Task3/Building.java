6b95ea7c9e00f17b4b72393a480845c7b4c98132
AndreasJensen1997 <planeten@live.dk>
2025-10-03 11:04:32 +0200

    instans metode

--- Torsdagsopgave 5/src/Task3/Building.java
+++ Torsdagsopgave 5/src/Task3/Building.java
@@ -4,13 +4,41 @@ import java.util.ArrayList;
 
 public class Building {
 
-    private ArrayList<Room> rooms;
+    private ArrayList<Room> rooms = new ArrayList<>();
     private int numberOfFloors;
 
 
-    public Building(ArrayList<Room> rooms, int numberOfFloors) {
-        this.rooms = rooms;
+    public Building(int numberOfFloors) {
+//       this.rooms = rooms;
         this.numberOfFloors = numberOfFloors;
+    }
+
+    public int countLampsInBuilding() {
+        int totalLamps = 0;
+
+        for (Room room : getRooms()) {
+            totalLamps += room.getNumberOfLamps();
+        }
+        return totalLamps;
+
+    }
+
+    public int countWindowsInBuilding() {
+        int totalWindows = 0;
+
+        for (Room room : getRooms()) {
+            totalWindows += room.getNumberOfWindows();
+        }
+        return totalWindows;
+
+    }
+
+    public void addRoom(Room room) {
+        rooms.add(room);
+    }
+
+    public int countRooms(){
+        return getRooms().size();
     }
 
     public ArrayList<Room> getRooms() {
